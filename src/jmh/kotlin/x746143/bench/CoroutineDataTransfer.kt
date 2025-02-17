3f9ff77eee2ba7b6a977bb9d260dc3c4fbca87e3
0x746143 <176037012+0x746143@users.noreply.github.com>
2025-02-17 15:18:32 +0100

    Move CoroutineDataTransfer to package x746143.bench

--- src/jmh/kotlin/x746143/bench/CoroutineDataTransfer.kt
+++ src/jmh/kotlin/x746143/bench/CoroutineDataTransfer.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package x746143
+package x746143.bench
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
