3e536c280c4674f45d42abed0483f1d75cb3c259
0x746143 <176037012+0x746143@users.noreply.github.com>
2025-02-18 16:59:46 +0100

    Create the Gradle task "compileJmhNative" to compile c-files by gcc

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,3 +53,41 @@ fun registerBenchmarkTask(className: String, vararg regex: String) {
         }
     }
 }
+
+abstract class ExecCommand : DefaultTask() {
+    @get:Inject
+    abstract val execOperations: ExecOperations
+
+    fun execCommand(vararg args: String): ExecResult {
+        return execOperations.exec {
+            commandLine(*args)
+        }
+    }
+}
+
+tasks["compileJmhKotlin"].dependsOn("compileJmhNative")
+
+tasks.register<ExecCommand>("compileJmhNative") {
+    val javaHome: String = System.getProperty("java.home")
+    val nativeDir = layout.buildDirectory.dir("native").get()
+    val sourceDir = layout.projectDirectory.dir("src/jmh/c")
+    val sourceFiles = fileTree(sourceDir) { include("**/*.c", "**/*.h") }
+    val libFiles = sourceFiles
+        .filter { it.name.startsWith("lib") }
+        .associate { src -> ("$nativeDir/${src.name.replace(".c", ".so")}") to src.absolutePath }
+
+    inputs.files(sourceFiles)
+    outputs.files(libFiles.keys)
+
+    doLast {
+        libFiles.forEach { (outFile, srcFile) ->
+            execCommand(
+                "gcc", "-shared", "-fPIC", "-O3", "-flto",
+                "-I$javaHome/include",
+                "-I$javaHome/include/linux",
+                "-I$sourceDir/openjdk/libjava",
+                "-o", outFile, srcFile
+            )
+        }
+    }
+}
